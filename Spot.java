9d902db9b2e1f044c4b45a983b3674a73c043245
Kashif Choudhry <kashif@attabotics.com>
2023-01-17 23:32:16 -0700

    First Commit

---
+++ Spot.java
@@ -0,0 +1,13 @@
+public class Spot {
+    public static void main(String[] args) {
+        int [] arr={1,3,5,12,14,16};
+
+        for (int i = 0; i < arr.length; i++) {
+            if( arr[i]>10){
+                System.out.println(arr[i]);
+            }
+
+        }
+
+    }
+}
